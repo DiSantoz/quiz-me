1574374c18ae159308b3371717ed7401db273c55
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2021-05-08 19:40:26 -0400

    corrected time displaying issues and response issue with correct or incorrect display

--- assets/script.js
+++ assets/script.js
@@ -1,109 +1,96 @@
-// timer DOM element
-var timerEl = document.querySelector("#timer");
-// question DOM element
-var questionEl = document.querySelector("#question");
-// choices DOM element (multiple choice)
-var choicesEl = document.querySelector("#choices");
-// results DOM element (correct or incorrect)
-var resultEl = document.querySelector("#result");
-// High score DOM element
-var hightScoreEl = document.querySelector("#viewHighScore");
-
-var questionIndex = 0;
-var highScore = 0;
-
-var time = 20;
-var intervalId;
-
-
-// array of all questions
 var questions = [
+    {
+      question: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts",
+    },
     {
         question: "Inside which HTML element do we put the JavaScript?",
         choices: ["<script>", "<scripting>", "<javascript>", "<js>"],
         answer: "<script>",
     },
     {
-        question: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts",
+      question:
+        "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses",
     },
-    {
-        question:
-            "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses",
-    },
-];
-
-// function to display question from array
-function displayQuestion() {
-
-    if (time == 0) {
-        updateTime();
-        return;
+  ];
+  
+  var questionEl = document.querySelector("#question");
+  var multipleChoiceEl = document.querySelector("#multipleChoice");
+  var questionResultEl = document.querySelector("#question-result");
+  var timerEl = document.querySelector("#timer");
+  
+  var questionIndex = 0;
+  var correctCount = 0;
+  
+  var time = 20;
+  var intervalId;
+  
+  function endQuiz() {
+    clearInterval(intervalId);
+    var body = document.body;
+    body.innerHTML = "Game over, You scored " + correctCount;
+  }
+  
+  function updateTime() {
+    time--;
+    timerEl.textContent = 'Time: ' + time;
+    if (time <= 0) {
+      endQuiz();
     }
-
+  }
+  
+  function displayQuestion() {
+    
+    if (time == 0) {
+      updateTime();
+      return;
+    }
+  
     intervalId = setInterval(updateTime, 1000);
-
+    
     questionEl.textContent = questions[questionIndex].question;
-
-    choicesEl.innerHTML = "";
-    resultEl.innerHTML = "";
-
+  
+    multipleChoiceEl.innerHTML = "";
+    questionResultEl.innerHTML = "";
+  
     var choices = questions[questionIndex].choices;
     var choicesLenth = choices.length;
-
+  
     for (var i = 0; i < choicesLenth; i++) {
-        var questionListItem = document.createElement("li");
-        var buttonChoicesEl = document.createElement("button");
-        buttonChoicesEl.textContent = choices[i];
-        buttonChoicesEl.className = "choices-list";
-        questionListItem.appendChild(buttonChoicesEl);
-        //questionListItem.textContent = choices[i];//
-        choicesEl.appendChild(questionListItem);
+      var questionListItem = document.createElement("li");
+      questionListItem.className = ("choices-list");
+      questionListItem.textContent = choices[i];
+      multipleChoiceEl.append(questionListItem);
     }
-}
-// next question will display as long as timer is not 0 or no questions remain
-function nextQuestion() {
+  }
+  
+  function nextQuestion() {
     questionIndex++;
     if (questionIndex === questions.length) {
-        endQuiz();
+      time = 0;
     }
     displayQuestion();
-}
-
-// verify if question is correct or incorrect and increase score or decrease time
-function checkAnswer(event) {
+  }
+  
+  function checkAnswer(event) {
     clearInterval(intervalId);
     if (event.target.matches("li")) {
-        var answer = event.target.textContent;
-        if (answer === questions[questionIndex].answer) {
-            resultEl.textContent = "Correct";
-            highScore++;
-        } else {
-            resultEl.textContent = "Incorrect";
-            time = time - 2;
-            timerEl.textContent = time;
-        }
+      var answer = event.target.textContent;
+      if (answer === questions[questionIndex].answer) {
+        questionResultEl.textContent = "Correct";
+        correctCount++;
+      } else {
+        questionResultEl.textContent = "Incorrect";
+        time = time - 2;
+        timerEl.textContent = time;
+      }
     }
     setTimeout(nextQuestion, 2000);
-}
-
-// function to end the quiz and show score
-function endQuiz() {
-    clearInterval(intervalId);
-    document.body.innerHTML = "Game over, You scored " + highScore;
-}
-// time will decrease by 1 second
-function updateTime() {
-    time--;
-    timerEl.textContent ='Time: ' + time;
-    if (time <= 0) {
-        endQuiz();
-    }
-}
-
-
-displayQuestion();
-choicesEl.addEventListener("click", checkAnswer);
+  }
+  
+  displayQuestion();
+  multipleChoiceEl.addEventListener("click", checkAnswer);
+  
